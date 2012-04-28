eb84bb9f1967ca727fc5ba7aee042673ae03a672
dwidmaye <dwidmaye@gmail.com>
2012-04-28 16:13:34 -0600

    added css and helpers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +15,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => " | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -22,7 +26,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector('title', :content => " | Contact")
+      response.should have_selector('title', :content => @base_title + " | Contact")
     end
   end
   
@@ -33,7 +37,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector('title', :content => " | About")
+      response.should have_selector('title', :content => @base_title + " | About")
     end
   end
 

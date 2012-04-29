eb808a7ae4a5f5500e71fcf76be8a57ea08c1dfa
dwidmaye <dwidmaye@gmail.com>
2012-04-29 09:32:13 -0600

    Added view helper and updated specs

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def logo
+    image_tag("logo.png", :alt => "Sample app", :class => 'round')
+  end
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -23,6 +23,21 @@ describe "LayoutLinks" do
   
   it "should have a Sign Up page at '/signup'" do
     get '/signup'
-    response.should have_selector("title", :content => 'Sign Up')
+    response.should have_selector("title", :content =>  "Sign Up")
+  end
+  
+  it "should have the right links to the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    visit root_path
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign Up")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
   end
 end

ee23d65339aa7433a071218797540f5cabacd37b
dwidmaye <dwidmaye@gmail.com>
2012-06-24 11:15:35 -0600

    Done with sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
   
+  match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
+   
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -9,7 +14,6 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match '/signup', :to => 'users#new'
   
   root :to => 'pages#home'
   # The priority is based upon order of creation:
